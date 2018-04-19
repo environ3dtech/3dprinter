0536cc39372b06fa34381115a1f434907971b310
jackthegenius1 <theinhtut.dec@gmail.com>
2018-04-19 12:17:56 +0800

    Delete all File

--- wp-content/themes/startup-company/woocommerce/cmsmasters-framework/theme-style/templates/global/wrapper-end.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @cmsmasters_package 	Startup Company
- * @cmsmasters_version 	1.0.1
- */
-
-
-echo '</div>' . "\n" . 
-'<!-- _________________________ Finish Content _________________________ -->' . "\n\n";
--- wp-content/themes/startup-company/woocommerce/cmsmasters-framework/theme-style/templates/loop/loop-start.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @cmsmasters_package 	Startup Company
- * @cmsmasters_version 	1.0.1
- */
-
-
-?>
-<ul class="products cmsmasters_products">
